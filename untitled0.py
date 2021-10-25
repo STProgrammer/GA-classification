c23a5f9563a8c4f3e9d05cea578bf7888ffd1265
Abdullah <aka160@post.uit.no>
2021-10-26 00:15:18 +0200

    neural network with 3 hidden layers test run, CEL decrease but acc is almost same

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 00:03:35 2021
+
+@author: abdka
+"""
+
+class DecisionTree():
+    def __init__(self):
+        return
+    
+    
+
+arr1 = [0, 1, 2, 3]
+
