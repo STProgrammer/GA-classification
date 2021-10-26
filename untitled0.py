437b7b35908e232b7ecf416296028daaa270a357
Abdullah <aka160@post.uit.no>
2021-10-27 01:21:26 +0200

    test with PyTorch

--- untitled0.py
+++ untitled0.py
@@ -4,10 +4,10 @@ Created on Tue Oct 26 00:03:35 2021
 
 @author: abdka
 """
-
+import numpy as np
 
 class SimpleBalancedDecisionTree():
-    def __init__(self, indices):
+    def __init__(self, indices, test_values):
         self.indices = indices
         self.test_values = test_values
         return
@@ -16,15 +16,15 @@ class SimpleBalancedDecisionTree():
         i = 0
         # Move to right if
         while i < len(indices):
-            if data_row[indices[i]] < test_values[i]:
+            if data_row[indices[i]] < self.test_values[i]:
                 i = (2*i)+2
-                # If leaf node
+                # If leaf node (also no more nodes)
                 if len(indices) <= i:
                     return 1
             # Move to left
             else:
                 i = (2*i)+1
-                # If leaf node
+                # If leaf node (also no more nodes)
                 if len(indices) <= i:
                     return 0
 
@@ -32,17 +32,19 @@ class SimpleBalancedDecisionTree():
 
 
 
-class Tree():
+class DecisionTree():
     def __init__(self, head):
         self.head = head
     
     # Adding nodes
     # The direction is an array containing zeros and ones
     # It describes how to navigate in the tree when placing a node
+    # 0 means to left, 1 means to right
     def add(self, node, directions):
         temp = self.head
         placed = False
         
+        # Repeat directions while not placed
         while not placed:
             for d in directions:
                 if d:
@@ -63,46 +65,81 @@ class Tree():
                         temp = temp.left
     
     
-    
-    
-            
-    
+    def predict(self, data_row):
+        temp = self.head
         
+        direction = temp.test(data_row)
+        
+        if direction:
+            temp = temp.right
+            # No nodes on the right
+            if temp == None:
+                return 1
+        else:
+            temp = temp.left
+            # No nodes on the left
+            if temp == None:
+                return 0
+    
+    def predict_all(self, data_set):
+        results = np.zeros(len(data_set))
+        for i in range(len(data_set)):
+            results[i] = self.predict[data_set[i]]
+    
+    def mutate_tree(self):
+        
+            
 
 
-generating nodes
 
-adding those nodes to the tree.
-repeating adding algorithm, e.g. [left, left, right] until empty leaf found
 
-each node has adding algorithm
-
-The tree has nodes.
 
 
 class DecisionNode():
-    def __init__(self):
-        self.parent
-        self.left
-        self.right
-        self.indices
-        self.parameters
-        self.test_value
+    def __init__(self, indices, parameters, test_value):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.indices = indices
+        self.parameters = parameters
+        self.test_value = test_value
 
     def test(self, data_row):
         sum_val = sum(data_row[self.indices]*self.parameters)
-        if sum_val < test_value:
-            i = (2*i)+2
-            # If leaf node
-            if len(indices) <= i:
-                return 1
-            # Move to left
+        if sum_val < self.test_value:
+            # Move to right
+            return 1
         else:
-            i = (2*i)+1
-            # If leaf node
-            if len(indices) <= i:
-                return 0
+            # Move to left
+            return 0
 
+
+
+
+def generate_random_tree(vocab_length):
+    import random
+    nr_of_nodes = np.random.randint(3, 15)
+    tree = None
+    for i in range(nr_of_nodes):
+        formula_length = np.random.randint(1,5)
+        indices = np.random.randint(0, vocab_length, formula_length)
+        parameters = np.random.uniform(0, 1, formula_length)
+        test_val = formula_length*np.random.uniform(0,5)
+        node = DecisionNode(indices, parameters, test_val)
+        directions = np.random.randint(0,2,6)
+        if tree == None:
+            tree = DecisionTree(node)
+        else:
+            tree.add(node, directions)
+    
+    return tree
+
+
+def mutate_tree
+        
+    
+    
+    
 
 arr1 = [0, 1, 2, 3]
 
@@ -110,12 +147,5 @@ indices = [0, 3, 2, 1, 3]
 values = [0, 3, 2, 1, 4]
 
 
-if indice i in arr is above,
-go to left node
-if no left. return negative
-else:
-    go to right node
-    if no right, return positive.:
 
 
-Generating tree
